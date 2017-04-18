2859718af053fbc1b45438ee7ef3c73af054cbc2
Koppel-Zhou <koppel.zhou@gmail.com>
2017-04-18 10:11:16 +0800

    课程设计提交
    完成了如下内容：
    1-1：从控制器或服务器读取数据
    1-2：为文本域添加双向数据绑定
    1-3：用户的头像、昵称、发表的文本、粉丝等信息实现双向数据绑定
    2-1：ng-repeat实现导航条
    3-1：粉丝排序显示
    4-1：路由实现最新动态视图
    4-2：路由实现简介视图
    4-3：路由实现消息视图

---
+++ app/app.js
@@ -0,0 +1,5 @@
+﻿// 入口文件
+var app=angular.module("myapp",['ngRoute']);
+app.controller("mainctrl",function($scope){
+	$scope.user={'head':'images/a0.jpg','name':'别无所求','job':'IT大牛','address':'石家庄，河北'};
+});
---
+++ app/route.js
@@ -0,0 +1,9 @@
+﻿// 添加路由
+
+app.config(['$routeProvider',function( $routeProvider ){
+  $routeProvider
+  .when('/',{templateUrl: 'view/dynamic.html'})
+  .when('/message',{templateUrl: 'view/message.html'})
+  .when('/introduction',{templateUrl: 'view/introduction.html'})
+  .otherwise({redirectTo:'view/404.html'});
+}]);
